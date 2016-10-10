7f7b31995077d5d9c656a94a074574c3322b4eca
Alvaro Gonzalez <cajanegra1@gmail.com>
2016-10-10 15:21:55 -0700

    Done with basics, turning to deep learning.

---
+++ numbers.py
@@ -0,0 +1,19 @@
+import math
+math.pi
+math.sqrt(45)
+
+import random
+help(random.random)
+random.random()
+random.choice([1, 2, 3, 4])
+help(random.choice)
+
+S = "shrubbery"
+L = list(S)
+L
+L[1] = "c"
+"".join(L)
+
+'%s, eggs, and %s' % ('spam', 'SPAM!')
+'{0}, eggs, and {1}'.format('spam', 'SPAM!')
+'{}, eggs, and {}'.format('spam', 'SPAM!')
--- script1.py
+++ script1.py
@@ -4,3 +4,14 @@ print(sys.platform)
 print(2 ** 100)         # Raise 2 to a power
 x = "Spam!"
 print(x * 8)            # String repetition
+
+L = [123, 'spam', 1.23]
+len(L)
+L[:-1]
+L + [4, 5, 6]
+L.pop(2)
+L
+help(L.insert)
+M = [[1, 2, 3], 
+    [4, 5, 6],
+    [7, 8, 9]]
